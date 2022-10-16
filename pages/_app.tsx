84bf43e4c6e730573a59c5f2b7cfcea7c5fd8e9b
Triston Wasik <twasik4@gmail.com>
2022-10-16 13:51:28 -0800

    ackee update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,15 +4,24 @@ import { NextUIProvider } from "@nextui-org/react";
 import * as ackeeTracker from "ackee-tracker";
 import { useEffect } from "react";
 
+export const ackeeInstance = ackeeTracker.create(
+  "https://analytics.cap.elfinslayer.io",
+  {
+    detailed: true,
+    ignoreLocalhost: true,
+    ignoreOwnVisits: false,
+  }
+);
+
 function MyApp({ Component, pageProps }: AppProps) {
   useEffect(() => {
-    const { stop } = ackeeTracker
-      .create("https://analytics.cap.elfinslayer.io", {
-        detailed: true,
-        ignoreLocalhost: true,
-        ignoreOwnVisits: false,
-      })
-      .record("6c742ae0-9f87-4b28-a350-a3a8e45d787d");
+    const { stop } = ackeeInstance.record(
+      "6c742ae0-9f87-4b28-a350-a3a8e45d787d",
+      {
+        siteLocation: window.location.href,
+        siteReferrer: document.referrer,
+      }
+    );
 
     return () => {
       stop();
--- pages/index.tsx
+++ pages/index.tsx
@@ -14,6 +14,7 @@ import styles from "../styles/Home.module.css";
 import axios from "axios";
 import { WorkshopItem } from "../helpers/types";
 import throttle from "lodash.throttle";
+import { ackeeInstance } from "./_app";
 
 const Home: NextPage = () => {
   // const test = "2809916332";
@@ -28,6 +29,10 @@ const Home: NextPage = () => {
         try {
           const { data } = await axios.post("api/scrape", { collectionId });
           const wkshpItems: WorkshopItem[] = data.items;
+          ackeeInstance.action("49e193a5-dc01-497b-812a-079b06ef71df", {
+            key: "Click",
+            value: 1,
+          });
 
           setMods(wkshpItems);
           setModIds(wkshpItems.map(({ id }) => id));
